dcfb0d2391231c92c6a7c703e596c916ee02a974
Jakob Notland <jakob.notland@ntnu.no>
2024-01-30 11:24:40 +0100

    Bootstrap and NAV

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import './App.css';
-import BTCSignals from './pictures/BTCSignals.png'
+import BTCSignals from './pictures/BTCSignals.png';
+import Logo from './logo.svg'
+import Research from './research/research'
+function HomePage() {
+  return <h2>Home Page</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <img src={BTCSignals}/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand as={Link} to="/">
+              <img
+                src={Logo}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="React Logo"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/research">Research</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Container style={{ marginTop: '20px' }}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/research" element={<Research />} />
+          </Routes>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/research/research.tsx
@@ -0,0 +1,6 @@
+
+function Research() {
+  return <h2>Research</h2>;
+}
+
+export default Research
